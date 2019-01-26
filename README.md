884517ab216fcceb53214cef4cbfe53ac9119927
selpo <planet_selpo@yahoo.co.jp>
2019-01-27 01:52:42 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# atcoder
+
+atcoder や codeforces での提出コードを commit 単位で管理
---
+++ atcoder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Math;
+class Z { static void Main() => new K(); }
+class K
+{
+	int F => int.Parse(Str);
+	long FL => long.Parse(Str);
+	int[] G => Strs.Select(int.Parse).ToArray();
+	long[] GL => Strs.Select(long.Parse).ToArray();
+	string Str => ReadLine();
+	string[] Strs => Str.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+	const int MOD = 1000000007;
+	public K()
+	{
+		SetOut(new StreamWriter(OpenStandardOutput()) { AutoFlush = false });
+		Solve();
+		Out.Flush();
+	}
+	void Solve()
+	{
+
+	}
+}
