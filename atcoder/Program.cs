ac0c74a8eb345563ef5d73ac97a6bc3acf4238aa
selpo <planet_selpo@yahoo.co.jp>
2019-01-27 17:35:13 +0900

    永続unionfind木 Union Sets https://code-thanks-festival-2017.contest.atcoder.jp/tasks/code_thanks_festival_2017_h

--- atcoder/Program.cs
+++ atcoder/Program.cs
@@ -1,161 +1,185 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 using static System.Console;
-using static System.Math;
-class Z { static void Main() => new K(); }
+using System.Collections.Generic;
+using System.Text;
+struct Pair
+{
+	public readonly int A, B;
+	public Pair(int a, int b) { A = a; B = b; }
+}
 class K
 {
-	int F => int.Parse(Str);
-	long FL => long.Parse(Str);
-	int[] G => Strs.Select(int.Parse).ToArray();
-	long[] GL => Strs.Select(long.Parse).ToArray();
-	string Str => ReadLine();
-	string[] Strs => Str.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-	const int MOD = 1000000007;
-	public K()
+	static int[] G => ReadLine().Split().Select(int.Parse).ToArray();
+	static void Main()
 	{
 		SetOut(new StreamWriter(OpenStandardOutput()) { AutoFlush = false });
-		Solve();
+		var I = G;
+		int N = I[0], M = I[1];
+		var es = new List<Pair>(M);
+		for (var e = 0; e < M; e++) { I = G; es.Add(new Pair(I[0] - 1, I[1] - 1)); }
+		var fuf = new FastUnionFindTree(N);
+		foreach (var e in es) fuf.UniteCategory(e.A, e.B);
+		var par = new int[N];
+		var repId = new Dictionary<int, int>();
+		for (var u = 0; u < N; u++) if (u == (par[u] = fuf.GetRootOf(u))) repId[u] = repId.Count;
+		fuf = null;
+		var R = repId.Count;
+		var id = new Dictionary<int, int>[R];
+		var es2 = new List<int>[R];
+		var qs2 = new HashSet<int>[R];
+		for (var r = 0; r < R; r++) { id[r] = new Dictionary<int, int>(); es2[r] = new List<int>(); qs2[r] = new HashSet<int>(); }
+		for (var u = 0; u < N; u++)
+		{
+			var r = repId[par[u]];
+			id[r][u] = id[r].Count;
+		}
+		for (var e = 0; e < M; e++) es2[repId[par[es[e].A]]].Add(e);
+		var Q = G[0];
+		var ans = new int[Q];
+		var qs = new Pair[Q];
+		for (var i = 0; i < Q; i++)
+		{
+			I = G; ans[i] = -1;
+			var q = qs[i] = new Pair(I[0] - 1, I[1] - 1);
+			var r = repId[par[q.A]];
+			if (par[q.A] == par[q.B]) { qs2[r].Add(i); qs[i] = new Pair(id[r][q.A], id[r][q.B]); }
+		}
+		repId.Clear();
+		for (var r = 0; r < R; r++)
+		{
+			if (qs2[r].Count == 0) continue;
+			if (qs2[r].Count <= 8 || id[r].Count <= 10)
+			{
+				fuf = new FastUnionFindTree(id[r].Count);
+				foreach (var i in es2[r])
+				{
+					int a = fuf.GetRootOf(id[r][es[i].A]), b = fuf.GetRootOf(id[r][es[i].B]);
+					if (a != b)
+					{
+						fuf.UniteCategory(a, b);
+						foreach (var q in qs2[r]) if (ans[q] < 0 && fuf.IsSameCategory(qs[q].A, qs[q].B)) ans[q] = i + 1;
+					}
+				}
+				qs2[r].Clear();
+				continue;
+			}
+			fuf = new FastUnionFindTree(id[r].Count);
+			var con = new List<UnionFindTree>(id[r].Count) { UnionFindTree.Create(id[r].Count) };
+			var ids = new List<int> { 0 };
+			foreach (var e in es2[r])
+			{
+				var last = con[con.Count - 1];
+				int a = fuf.GetRootOf(id[r][es[e].A]), b = fuf.GetRootOf(id[r][es[e].B]);
+				if (a != b)
+				{
+					ids.Add(e + 1);
+					fuf.UniteCategory(a, b);
+					con.Add(last.Unite(a, b));
+				}
+			}
+			fuf = null; es2[r].Clear();
+			foreach (var q in qs2[r])
+			{
+				int left = 0, right = con.Count;
+				while (left < right)
+				{
+					var i = (left + right) / 2;
+					int a = con[i][qs[q].A], b = con[i][qs[q].B];
+					if (a == b) right = i;
+					else left = i + 1;
+				}
+				ans[q] = ids[left];
+			}
+			con.Clear(); ids.Clear(); qs2[r].Clear();
+		}
+		var sb = new StringBuilder();
+		for (var i = 0; i < Q; i++) sb.AppendLine(ans[i].ToString());
+		Write(sb);
 		Out.Flush();
 	}
-	int Y, X;
-	List<GraphEdge> es;
-	void Solve()
+}
+struct UnionFindTree
+{
+	public int this[int x]
 	{
-		var I = G;
-		Y = I[0]; X = I[1];
-		var map = new bool[X, Y];
-		int sx = 0, sy = 0, tx = 0, ty = 0;
-		for (var y = 0; y < Y; y++)
+		get
 		{
-			var s = Str;
-			for (var x = 0; x < X; x++)
-			{
-				switch (s[x])
-				{
-					case 'S': sx = x; sy = y; break;
-					case 'T': tx = x; ty = y; break;
-				}
-				map[x, y] = s[x] == 'o';
-			}
+			int y;
+			while ((y = par.At(x)) != x) x = y;
+			return x;
 		}
-		var V = X + Y + 2;
-		es = new List<GraphEdge>();
-		Add(S, vX(sx), Func.InfL);
-		Add(S, vY(sy), Func.InfL);
-		Add(vX(tx), T, Func.InfL);
-		Add(vY(ty), T, Func.InfL);
-		for (var x = 0; x < X; x++) for (var y = 0; y < Y; y++) if (map[x, y]) Add2(vX(x), vY(y), 1);
-		var max = Func.CalcMaxFlow(V, S, T, es);
-		WriteLine(max >= Func.InfL ? -1 : max);
 	}
-	void Add(int f, int t, long c) { es.Add(new GraphEdge(f, t, c)); }
-	void Add2(int f, int t, long c) { Add(f, t, c); Add(t, f, c); }
-	int S => 0;
-	int T => X + Y + 1;
-	int vX(int x) => x + 1;
-	int vY(int y) => X + y + 1;
-}
-struct GraphEdge
-{
-	public readonly int From, To;
-	public readonly long Capacity;
-	public GraphEdge(int f, int t, long c)
+	readonly Tree par, rank;
+	UnionFindTree(Tree p, Tree r) { par = p; rank = r; }
+	public UnionFindTree Unite(int x, int y)
 	{
-		From = f;
-		To = t;
-		Capacity = c;
+		int rx = rank.At(x), ry = rank.At(y);
+		if (rx < ry) return new UnionFindTree(par.Set(x, y), rank);
+		return new UnionFindTree(par.Set(y, x), rx == ry ? rank.Set(x, rx + 1) : rank);
 	}
-}
-class MFEdge
-{
-	public int To;
-	public long Capacity;
-	public MFEdge Reverse;
-	public MFEdge(int to, long cap, MFEdge rev)
+	static Dictionary<int, UnionFindTree> memo = new Dictionary<int, UnionFindTree>();
+	public static UnionFindTree Create(int N)
 	{
-		To = to;
-		Capacity = cap;
-		Reverse = rev;
-	}
-}
-static class Func
-{
-	public const int Inf = 1073741789;
-	// 2 * Inf < int.MaxValue, and Inf is a prime number
-	public const long InfL = 4011686018427387913L;
-	// 2 * InfL < long.MaxValue, and InfL is a prime number
-	public static long CalcMaxFlow(int V, int source, int sink, List<GraphEdge> es)
-	{
-		var mes = new List<MFEdge>[V];
-		for (var u = 0; u < V; u++) mes[u] = new List<MFEdge>();
-		foreach (var e in es)
+		if (!memo.ContainsKey(N))
 		{
-			var e1 = new MFEdge(e.To, e.Capacity, null);
-			var e2 = new MFEdge(e.From, 0, e1);
-			e1.Reverse = e2;
-			mes[e.From].Add(e1);
-			mes[e.To].Add(e2);
+			var a = new int[N];
+			var r = Tree.Create(a, 0, N);
+			for (var i = 1; i < N; i++) a[i] = i;
+			var p = Tree.Create(a, 0, N);
+			memo[N] = new UnionFindTree(p, r);
 		}
-		return MaxFlow(source, sink, mes);
+		return memo[N];
 	}
-	static long MaxFlow(int s, int t, List<MFEdge>[] es)
+}
+class FastUnionFindTree
+{
+	readonly int[] rank, par;
+	public FastUnionFindTree(int N)
 	{
-		var V = es.Length;
-		var level = new int[V];
-		var flow = 0L;
+		par = new int[N];
+		rank = new int[N];
+		for (var i = 1; i < N; i++) par[i] = i;
+	}
+	public int GetRootOf(int x) => par[x] == x ? x : par[x] = GetRootOf(par[x]);
+	public bool UniteCategory(int x, int y)
+	{
+		if ((x = GetRootOf(x)) == (y = GetRootOf(y))) return false;
+		if (rank[x] < rank[y]) par[x] = y;
+		else
+		{
+			par[y] = x;
+			if (rank[x] == rank[y]) rank[x]++;
+		}
+		return true;
+	}
+	public bool IsSameCategory(int x, int y) => GetRootOf(x) == GetRootOf(y);
+}
+class Tree
+{
+	readonly Tree L, R;
+	readonly int I, X;
+	public Tree(Tree l, int i, int x, Tree r) { L = l; X = x; I = i; R = r; }
+	public int At(int i)
+	{
+		var t = this;
 		while (true)
-		{
-			MFBFS(s, level, es);
-			if (level[t] < 0) return flow;
-			var iter = new int[V];
-			while (true)
-			{
-				var f = MFDFS(s, t, InfL, level, iter, es);
-				if (f <= 0) break;
-				else flow += f;
-			}
-		}
+			if (t.I > i) t = t.L;
+			else if (t.I < i) t = t.R;
+			else return t.X;
 	}
-	static long MFDFS(int v, int t, long f, int[] level, int[] iter, List<MFEdge>[] es)
+	public Tree Set(int i, int x)
 	{
-		if (v == t) return f;
-		for (; iter[v] < es[v].Count; iter[v]++)
-		{
-			var e = es[v][iter[v]];
-			if (level[v] >= level[e.To] || e.Capacity <= 0) continue;
-			var g = MFDFS(e.To, t, Min(e.Capacity, f), level, iter, es);
-			if (g > 0)
-			{
-				e.Capacity -= g;
-				e.Reverse.Capacity += g;
-				return g;
-			}
-		}
-		return 0;
+		if (I > i) return new Tree(L.Set(i, x), I, X, R);
+		if (I < i) return new Tree(L, I, X, R.Set(i, x));
+		return new Tree(L, i, x, R);
 	}
-	static void MFBFS(int s, int[] level, List<MFEdge>[] es)
+	public static Tree Create(int[] list, int l, int r)
 	{
-		var V = es.Length;
-		for (var i = 0; i < V; i++) level[i] = -1;
-		var queue = new Queue<int>();
-		level[s] = 0;
-		queue.Enqueue(s);
-		while (queue.Count > 0)
-		{
-			var v = queue.Dequeue();
-			foreach (var e in es[v])
-				if (e.Capacity > 0 && level[e.To] < 0)
-				{
-					level[e.To] = level[v] + 1;
-					queue.Enqueue(e.To);
-				}
-		}
+		if (r - l == 1) return new Tree(null, l, list[l], null);
+		if (r - l == 2) return new Tree(new Tree(null, l, list[l], null), l + 1, list[l + 1], null);
+		if (r - l == 3) return new Tree(new Tree(null, l, list[l], null), l + 1, list[l + 1], new Tree(null, l + 2, list[l + 2], null));
+		var n = (l + r) / 2;
+		return new Tree(Create(list, l, n), n, list[n], Create(list, n + 1, r));
 	}
 }
