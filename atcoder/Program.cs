08d691bd1f8b9c8e75f0f65f70c2792a85a5a040
selpo <planet_selpo@yahoo.co.jp>
2019-01-28 01:04:17 +0900

    トポロジカルソート Restore the Tree https://atcoder.jp/contests/nikkei2019-qual/tasks/nikkei2019_qual_d

--- atcoder/Program.cs
+++ atcoder/Program.cs
@@ -24,23 +24,42 @@ class K
 		Solve();
 		Out.Flush();
 	}
+	int V;
+	Pair[] es;
 	void Solve()
 	{
-		var N = F;
-		var t = new Pair[N];
-		for (var i = 0; i < N; i++) { var I = GL; t[i] = new Pair(I[0], I[1]); }
-		Array.Sort(t, (x, y) => -(x.X + x.Y).CompareTo(y.X + y.Y));
-		var ans = 0L;
-		for (var i = 0; i < N; i++)
+		var I = G;
+		V = I[0]; var E = I[1] + V - 1;
+		es = new Pair[E];
+		for (var e = 0; e < E; e++) { I = G; es[e] = new Pair(I[0] - 1, I[1] - 1); }
+		var topo = TopologicalSort(V, es);
+		var rev = new Dictionary<int, int>();
+		for (var i = 0; i < V; i++) rev[topo[i]] = i;
+		var par = new int[V];
+		for (var i = 0; i < V; i++) par[i] = -1;
+		foreach (var e in es) par[e.Y] = Max(par[e.Y], rev[e.X]);
+		for (var i = 0; i < V; i++) WriteLine(par[i] < 0 ? 0 : topo[par[i]] + 1);
+	}
+	static List<int> TopologicalSort(int V, Pair[] es)
+	{
+		var topo = new List<int>();
+		var s = new Queue<int>();
+		var g = new List<int>[V];
+		var rg = new int[V];
+		for (var i = 0; i < V; i++) g[i] = new List<int>();
+		foreach (var e in es) { g[e.X].Add(e.Y); rg[e.Y]++; }
+		for (var i = 0; i < V; i++) if (rg[i] == 0) s.Enqueue(i);
+		while (s.Count > 0)
 		{
-			if (i % 2 == 0) ans += t[i].X;
-			else ans -= t[i].Y;
+			var n = s.Dequeue();
+			topo.Add(n);
+			foreach (var m in g[n]) if (--rg[m] == 0) s.Enqueue(m);
 		}
-		WriteLine(ans);
+		return topo.Count == V ? topo : null;
 	}
 }
 struct Pair
 {
-	public readonly long X, Y;
-	public Pair(long x, long y) { X = x; Y = y; }
+	public readonly int X, Y;
+	public Pair(int x, int y) { X = x; Y = y; }
 }
